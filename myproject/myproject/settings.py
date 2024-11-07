fd7697bff7b41e90a3d7c9c309ad7ad8b0bd9a89
Nanoka01 <146633767+Nanoka01@users.noreply.github.com>
2024-11-07 13:18:59 +0700

    เพิ่มภาพ-lastpage

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -132,3 +132,12 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'myapp/static')
 ]
+
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
