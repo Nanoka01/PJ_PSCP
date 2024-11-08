2fb6427a829610bbe187b274d8a35db5ec11ef5b
Nanoka01 <146633767+Nanoka01@users.noreply.github.com>
2024-11-08 21:17:58 +0700

    vercel

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-=t93#k9lu_p!5c$zytl3lm21b-8iehnf^cbp#(l(iu-ecu61#*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
